462341d548a051d32534028393842021bbbc196d
Kenywil Tiu <tiukenywil11@gmail.com>
2022-05-07 14:55:42 +0800

    Part 3: Added base Swagger

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const low = require("lowdb");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 const booksRouter = require('./routes/books');
 
 const PORT = process.env.PORT || 4000;
@@ -13,7 +15,26 @@ const db = low(adapter);
 
 db.defaults({ books: []}).write()
 
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Library API",
+			version: "1.0.0",
+			description: "A simple Express Library API"
+		},
+		servers: [{
+			url: "http://localhost:4000"
+		}],
+	},
+	apis: ["*./routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
 const app = express();
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.db = db;
 
